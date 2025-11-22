17ff162a749453ba2617befb66ec85e85f06a36b
twakj <twakjaco@hotmail.com>
2025-11-22 10:44:16 -0500

    add zip command line parameter

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,9 @@
-if (Test-Path .\ajwa.zip) { Remove-Item .\ajwa.zip -Force }
-Compress-Archive -Path .\app.js, .\package.json, .\package-lock.json -DestinationPath .\ajwa.zip
+param(
+	[switch]$Zip = $true
+)
 
+if ($Zip) {
+	if (Test-Path .\ajwa.zip) { Remove-Item .\ajwa.zip -Force }
+	Compress-Archive -Path .\app.js, .\package.json, .\package-lock.json -DestinationPath .\ajwa.zip
+	Write-Host "ajwa.zip recreated."
+} 
